ca536b611a9e08be7cba5958fb9c146dedff07b4
NoobHubV1 <163002311+NoobHubV1@users.noreply.github.com>
2024-03-15 10:14:23 +0700

    Update Anime Ball.lua

--- Anime Ball.lua
+++ Anime Ball.lua
@@ -1,3 +1,19 @@
---Red Circle Auto-Block:
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local PhantomForcesWindow = Library:NewWindow("NoobHubV1 Hub")
+
+local AnimeBall = PhantomForcesWindow:NewSection("Main")
+
+AnimeBall:CreateToggle("Auto Parry", function()--Red Circle Auto-Block:
 getgenv().visualizer = true
 loadstring(Game:HttpGet('https://raw.githubusercontent.com/1f0yt/community/main/Circle'))()
+end)
+
+AnimeBall:CreateToggle("Fps Boost", function()loadstring(game:HttpGet("https://shz.al/KWfS"))()
+end)
+
+AnimeBall:CreateToggle("KeyBoard", function()loadstring(game:HttpGet("https://gist.githubusercontent.com/RedZenXYZ/4d80bfd70ee27000660e4bfa7509c667/raw/da903c570249ab3c0c1a74f3467260972c3d87e6/KeyBoard%2520From%2520Ohio%2520Fr%2520Fr"))()
+end)
+
+AnimeBall:CreateToggle("FFJ Hub Anime Ball", function()loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/AnimeBall.lua"))()
+end)
