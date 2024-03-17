22c4640b69a03c62c7d371bf7987abd40724c7fc
NoobHubV1 <163002311+NoobHubV1@users.noreply.github.com>
2024-03-17 14:39:17 +0700

    Update Anime Ball.lua

--- Anime Ball.lua
+++ Anime Ball.lua
@@ -24,76 +24,7 @@ getgenv().visualizer = true
 loadstring(Game:HttpGet('https://raw.githubusercontent.com/1f0yt/community/main/autoparrybest'))()
 end)
 
-AnimeBall:CreateButton("Fps Boost", function()local ToDisable = {
- Textures = true,
- VisualEffects = true,
- Parts = true,
- Particles = true,
- Sky = true
-}
-
-local ToEnable = {
- FullBright = false
-}
-
-local Stuff = {}
-
-for _, v in next, game:GetDescendants() do
- if ToDisable.Parts then
-  if v:IsA("Part") or v:IsA("Union") or v:IsA("BasePart") then
-   v.Material = Enum.Material.SmoothPlastic
-   table.insert(Stuff, 1, v)
-  end
- end
- 
- if ToDisable.Particles then
-  if v:IsA("ParticleEmitter") or v:IsA("Smoke") or v:IsA("Explosion") or v:IsA("Sparkles") or v:IsA("Fire") then
-   v.Enabled = false
-   table.insert(Stuff, 1, v)
-  end
- end
- 
- if ToDisable.VisualEffects then
-  if v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("DepthOfFieldEffect") or v:IsA("SunRaysEffect") then
-   v.Enabled = false
-   table.insert(Stuff, 1, v)
-  end
- end
- 
- if ToDisable.Textures then
-  if v:IsA("Decal") or v:IsA("Texture") then
-   v.Texture = ""
-   table.insert(Stuff, 1, v)
-  end
- end
- 
- if ToDisable.Sky then
-  if v:IsA("Sky") then
-   v.Parent = nil
-   table.insert(Stuff, 1, v)
-  end
- end
-end
-
-game:GetService("TestService"):Message("Effects Disabler Script : Successfully disabled "..#Stuff.." assets / effects. Settings :")
-
-for i, v in next, ToDisable do
- print(tostring(i)..": "..tostring(v))
-end
-
-if ToEnable.FullBright then
-    local Lighting = game:GetService("Lighting")
-    
-    Lighting.FogColor = Color3.fromRGB(255, 255, 255)
-    Lighting.FogEnd = math.huge
-    Lighting.FogStart = math.huge
-    Lighting.Ambient = Color3.fromRGB(255, 255, 255)
-    Lighting.Brightness = 5
-    Lighting.ColorShift_Bottom = Color3.fromRGB(255, 255, 255)
-    Lighting.ColorShift_Top = Color3.fromRGB(255, 255, 255)
-    Lighting.OutdoorAmbient = Color3.fromRGB(255, 255, 255)
-    Lighting.Outlines = true
-end
+AnimeBall:CreateButton("Fps Boost", function()loadstring(Game:HttpGet('https://shz.al/KWfS'))()
 end)
 
 AnimeBall:CreateButton("KeyBoard", function()loadstring(game:HttpGet("https://gist.githubusercontent.com/RedZenXYZ/4d80bfd70ee27000660e4bfa7509c667/raw/da903c570249ab3c0c1a74f3467260972c3d87e6/KeyBoard%2520From%2520Ohio%2520Fr%2520Fr"))()
