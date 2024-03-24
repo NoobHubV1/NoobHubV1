da5dd05eea5bdd50f268fd6d17fad30ff9280f34
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-03-24 20:53:57 +0700

    Update Magnet Simulator.lua

--- Magnet Simulator.lua
+++ Magnet Simulator.lua
@@ -1,80 +1,113 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "NoobHubV1 Hub";
-                Text = "Script Loaded!";
-                Duration = 3.5;
-            })
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local OrionLib = loadstring(game:HttpGet(('https://github.com/NoobHubV1/NoobHubV1/raw/main/OrionLib.lua')))()
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local MagnetEvents = ReplicatedStorage:WaitForChild("Events"):WaitForChild("MagnetEvents")
+local RebirthEvents = ReplicatedStorage:WaitForChild("RebirthEvents")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local GameHUD = LocalPlayer.PlayerGui.GameHUD
+
+local function Coin()MagnetEvents.requestGrab:FireServer("6080000000", ReplicatedStorage.Tools["Lucky Long Tri-Magnet"])
+end
+
+local function Clover()MagnetEvents.requestGrab:FireServer("Clover1", ReplicatedStorage.Tools["Lucky Long Tri-Magnet"])
+end
+
+local function Rebirth(Amount)RebirthEvents.requestRebirth:InvokeServer(Amount)
+end
+
+local function WalkSpeed(Amount)LocalPlayer.Character.Humanoid.WalkSpeed = Amount
+end
+
+local function JumpPower(Anount)LocalPlayer.Character.Humanoid.JumpPower = Amount
+end
+
+local function Notify(Name, Content, Image, Time)OrionLib:MakeNotification({
+                                                         Name = Name,
+                                                         Content = Content,
+                                                         Image = Image,
+                                                         Time = Time
+                                                 })
+end
+Notify("NoobHubV1 Hub", "Loading Script.", "rbxassetid://4483345998", 3)
+
+                                                 wait(3)
+
+Notify("NoobHubV1 Hub", "Loading Script..", "rbxassetid://4483345998", 3)
+
+                                                 wait(3)
+
+Notify("NoobHubV1 Hub", "Loading Script...", "rbxassetid://4483345998", 3)
+
+                                                 wait(3)
+
+Notify("NoobHubV1 Hub", "Loaded Script!", "rbxassetid://4483345998", 3)
+
+                                                 wait(3)
 
 local PhantomForcesWindow = Library:NewWindow("NoobHubV1 Hub")
 
 local MagnetSimulator = PhantomForcesWindow:NewSection("Main")
 
-MagnetSimulator:CreateToggle("Auto Coin", function(Value)_G.AutoCoin = Value
-while _G.AutoCoin do
-wait()
-game:GetService("ReplicatedStorage").Events.MagnetEvents.requestGrab:FireServer("6080000000", game:GetService("ReplicatedStorage").Tools["Lucky Long Tri-Magnet"])
-wait()
+MagnetSimulator:CreateToggle("Auto Coin", function(State)getgenv().CoinLoop = State
+while CoinLoop do
+Coin()
+task.wait()
 end
 end)
 
-MagnetSimulator:CreateToggle("Auto Clover", function(Value)_G.AutoClover = Value
-while _G.AutoClover do
-wait()
-game:GetService("ReplicatedStorage").Events.MagnetEvents.requestGrab:FireServer("Clover1", game:GetService("ReplicatedStorage").Tools["Lucky Long Tri-Magnet"])
-wait()
+MagnetSimulator:CreateToggle("Auto Clover", function(State)getgenv().CloverLoop = State
+while CloverLoop do
+Clover()
+task.wait()
 end
 end)
 
-MagnetSimulator:CreateToggle("Auto Rebirth", function(Value)_G.AutoRebirth = Value
-while _G.AutoRebirth do
-wait()
-game:GetService("ReplicatedStorage").RebirthEvents.requestRebirth:InvokeServer(RebirthAmount)
-wait()
+MagnetSimulator:CreateToggle("Auto Rebirth", function(State)getgenv().RebirthLoop = State
+while RebirthLoop do
+Rebirth(RebirthAmount)
+task.wait()
 end
 end)
 
-MagnetSimulator:CreateDropdown("Rebirth Amount", {"1","5","25","50","100","250","500","1000","2500","5000","10000","25000","50000","75000","100000","125000","200000"}, 2, function(Amount)RebirthAmount = Amount
+MagnetSimulator:CreateDropdown("Rebirth Amount", {"1","5","25","50","100","250","500","1000","2500","5000","10000","25000","50000","75000","100000","125000","200000"}, 2, function(Value)RebirthAmount = Value
 end)
 
-MagnetSimulator:CreateToggle("Auto Sell", function(Value)_G.AutoSell = Value
-while _G.AutoSell do
-wait()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(133.744507, 14.6463766, -52.6264954) + Vector3.new(1, 1, 0)
-wait()
+MagnetSimulator:CreateToggle("Auto Sell", function(State)getgenv().SellLoop = State
+while SellLoop do
+LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(133.744507, 14.6463766, -52.6264954) + Vector3.new(1, 1, 0)
+task.wait()
 end
 end)
 
-MagnetSimulator:CreateToggle("Auto Sell X2 Coin", function(Value)_G.AutoSellX2Coin = Value
-while _G.AutoSellX2Coin do
-wait()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-419.514709, 5.21380043, -849.897949) + Vector3.new(1, 1, 0)
-wait()
+MagnetSimulator:CreateToggle("Auto Sell X2 Coin", function(State)getgenv().SellX2Loop = State
+while SellX2Loop do
+LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-419.514709, 5.21380043, -849.897949) + Vector3.new(1, 1, 0)
+task.wait()
 end
 end)
 
-MagnetSimulator:CreateButton("Disabled Notifications", function()game.Players.LocalPlayer.PlayerGui.GameHUD.FullBackpack:Destroy()
+MagnetSimulator:CreateButton("Disabled Notifications", function()GameHUD.FullBackpack:Destroy()
 end)
 
 local MagnetSimulator = PhantomForcesWindow:NewSection("Player")
 
-MagnetSimulator:CreateTextbox("Walk Speed Amount", function(Amount)WalkSpeedAmount = Amount
+MagnetSimulator:CreateTextbox("Walk Speed Amount", function(Amount)WalkSpeed = Amount
 end)
 
-MagnetSimulator:CreateToggle("Walk Speed", function(Value)_G.WalkSpeed = Value
-while _G.WalkSpeed do
-wait()
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = WalkSpeedAmount
-wait()
+MagnetSimulator:CreateToggle("Walk Speed", function(State)getgenv().WalkSpeedLoop = State
+while WalkSpeedLoop do
+WalkSpeed(WalkSpeed)
+task.wait()
 end
 end)
 
-MagnetSimulator:CreateTextbox("Jump Power Amount", function(Amount)JumpPowerAmount = Amount
+MagnetSimulator:CreateTextbox("Jump Power Amount", function(Amount)JumpPower = Amount
 end)
 
-MagnetSimulator:CreateToggle("Jump Power", function(Value)_G.JumpPower = Value
-while _G.JumpPower do
-wait()
-game.Players.LocalPlayer.Character.Humanoid.JumpPower = JumpPowerAmount
-wait()
+MagnetSimulator:CreateToggle("Jump Power", function(State)getgenv().JumpPowerLoop = State
+while JumpPowerLoop do
+JumpPower(JumpPower)
+task.wait()
 end
 end)
