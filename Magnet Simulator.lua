199c676b665ff3c9aa8feb0408eeac27c8786c56
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-03-25 20:07:24 +0700

    Update Magnet Simulator.lua

--- Magnet Simulator.lua
+++ Magnet Simulator.lua
@@ -23,6 +23,107 @@ local function AutoSell(Sell)if Sell == "X2 Coin" then
                              end
 end
 
+local function InfBadge()--[[  
+Made by NoobHubV1
+]]
+_G.farm = not _G.farm
+if _G.farm then 
+	print("----------------------------")
+	print("Farming Enabled")
+	print("----------------------------")
+else 
+	print("----------------------------")	
+	print("Farming Disabled")
+	print("----------------------------")
+end
+local plr = game.Players.LocalPlayer
+local char = plr.Character
+local rs = game:GetService("RunService").RenderStepped
+local sellring = game.Workspace.Rings:FindFirstChild("Sellx2")
+local RebirthMagnets = plr.PlayerGui.RebirthShop.Holder.Pages:GetChildren()
+local maxmultiplier = 0
+local bestmagnet
+for i,v in pairs (RebirthMagnets) do 
+	if v:FindFirstChild("Stats") then 
+		if v.Stats:FindFirstChild("Multiplier") then 
+			local text = v.Stats.Multiplier.Text
+			local magnet = v.Middle.ItemName.Text
+			local start = string.find(text, "%d")
+			local textmultiplier = string.sub(string.sub(text,start),0,string.find(string.sub(text,start), "x")-1)
+			local multiplier = tonumber(textmultiplier)
+			if multiplier >= maxmultiplier then 
+				maxmultiplier = multiplier
+				bestmagnet = magnet
+			end
+		end
+	end
+end
+
+local Tool = game.ReplicatedStorage.Tools:FindFirstChild(bestmagnet)
+local grab = game.ReplicatedStorage.Events.MagnetEvents.requestGrab
+if not _G.firstlaunch then
+	for _,v in pairs(plr.Purchases:GetChildren()) do -- Give Gamepasses (most of them are only visual and dont work, but some might really work)
+		v.Value = true
+	end
+	sellring.Transparency = 1
+	_G.firstlaunch = true
+	if plr.PlayerGui.GameHUD:FindFirstChild("FullBackpack") then
+		plr.PlayerGui.GameHUD.FullBackpack:Destroy()
+	end
+end
+if _G.farm then
+	if Tool then
+		print()
+		print("Found Best Magnet : ",bestmagnet)
+		print()
+		Tool.Parent = plr.Backpack
+	elseif plr.Backpack:FindFirstChild(bestmagnet) then
+		print()
+		print("The",bestmagnet, "is in your Backpack")
+		print()
+	elseif plr.Character:FindFirstChild(bestmagnet) then
+		print()
+		print("You are holding the",bestmagnet)
+		print()
+	else
+		print()
+		print("The",bestmagnet, "is missing from the ReplicatedStorage, Try restarting your game")
+		print()
+	end
+end
+while _G.farm do
+	rs:wait()
+	if plr.Backpack:FindFirstChild(bestmagnet) then
+		grab:FireServer("65",plr.Backpack[bestmagnet])
+	elseif plr.Character:FindFirstChild(bestmagnet) then
+		grab:FireServer("65",plr.Character[bestmagnet])
+	end
+	sellring.CFrame = char.HumanoidRootPart.CFrame + Vector3.new(0,math.random(-1,1),0)
+end
+
+char.Humanoid.Died:Connect(function()
+	_G.firstlaunch = false
+	_G.farm = false
+	if plr.Backpack:FindFirstChild(bestmagnet) then
+		plr.Backpack:FindFirstChild(bestmagnet).Parent = game.ReplicatedStorage.Tools
+	elseif plr.Character:FindFirstChild(bestmagnet) then
+		plr.Character:FindFirstChild(bestmagnet).Parent = game.ReplicatedStorage.Tools
+	end
+end)
+
+char.Humanoid.StateChanged:Connect(function(oldState, newState)
+	if newState == Enum.HumanoidStateType.Dead then
+		_G.firstlaunch = false
+		_G.farm = false
+		if plr.Backpack:FindFirstChild(bestmagnet) then
+			plr.Backpack:FindFirstChild(bestmagnet).Parent = game.ReplicatedStorage.Tools
+		elseif plr.Character:FindFirstChild(bestmagnet) then
+			plr.Character:FindFirstChild(bestmagnet).Parent = game.ReplicatedStorage.Tools
+		end
+	end
+end)
+end
+
 local function Notify(Name, Content, Image, Time)OrionLib:MakeNotification({
                                                          Name = Name,
                                                          Content = Content,
@@ -82,6 +183,9 @@ end)
 
 MagnetSimulator:CreateButton("Disabled Notifications", function()GameHUD.FullBackpack:Destroy()
 end)
+
+MagnetSimulator:CreateButton("Inf Badge", function()InfBadge()
+end
 
 local MagnetSimulator = PhantomForcesWindow:NewSection("Player")
 
