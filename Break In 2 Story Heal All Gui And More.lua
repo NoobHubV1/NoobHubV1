1b519b0764cd1fe3d6bb3b7f5a2af465fafb95c3
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-03-26 17:32:42 +0700

    Update and rename Break In 2 Story Heal All Gui.lua to Break In 2 Story Heal All Gui And More.lua

--- Break In 2 Story Heal All Gui And More.lua
+++ Break In 2 Story Heal All Gui And More.lua
@@ -4,6 +4,9 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Events = ReplicatedStorage:WaitForChild("Events")
+local HealTheNoobs = Events.HealTheNoobs
+
+local ItemsTable = {"Crowbar 1","Crowbar 2","Crowbar 3","Bat","Pitchfork","Hammer","Wrench","Broom","Med Kit","Key","Gold Key","Louise","Lollipop","Chips","Golden Apple","Pizza","Gold Pizza","Rainbow Pizza","Rainbow Pizza Box","Book","Phone","Cookie","Apple","Bloxy Cola","Expired Bloxy Cola","Bottle","Ladder","Battery"}
 
 local function UnequipAllTools()for i, v in pairs(LocalPlayer.Character:GetChildren()) do
 			if v:IsA("Tool") then
@@ -15,10 +18,18 @@ end
 local function GiveItem(Item)Events:WaitForChild("GiveTool"):FireServer(tostring(Item:gsub(" ", "")))
 end
 
-local function EquipItem(Item)LocalPlayer.Backpack:WaitForChild(tostring(Item:gsub(" ", ""))).Parent = LocalPlayer.Character
+local function EquipItem(Item)if Item == "Golden Apple" then
+		              LocalPlayer.Backpack:WaitForChild("GoldenApple").Parent = LocalPlayer.Character
+		              task.wait(.1)
+		              HealTheNoobs.FireServer(LocalPlayer)
+	                      else
+		              LocalPlayer.Backpack:WaitForChild(tostring(Item:gsub(" ", ""))).Parent = LocalPlayer.Character
 end
 
-local function HealTheNoobs()Events.HealTheNoobs:FireServer()
+local function RemoveItem(Item)LocalPlayer.Backpack:WaitForChild(tostring(Item:gsub(" ", ""))):Destroy()
+end
+
+local function UnequipItem(Item)LocalPlayer.Character:WaitForChild(tostring(Item:gsub(" ", ""))).Parent = LocalPlayer.Backpack
 end
 
 local function HealAllPlayers()UnequipAllTools()
@@ -27,7 +38,7 @@ local function HealAllPlayers()UnequipAllTools()
                                task.wait(.1)
                                EquipItem("Golden Apple")
                                task.wait(.1)
-                               HealTheNoobs()
+                               HealTheNoobs:FireServer(LocalPlayer)
 end
 
 local function Notify(Name, Content, Image, Time)OrionLib:MakeNotification({
@@ -62,5 +73,29 @@ HealAllPlayers()
 task.wait(.1)
 end
 end)
+
+local BreakIn2 = PhantomForcesWindow:NewSection("Remove Item")
+
+BreakIn2:CreateDropdown("Remove Item", ItemsTable, 2, function(Value)SelectedItemRemove = Value
+end)
+
+BreakIn2:CreateButton("Remove", function()RemoveItem(SelectedItemRemove)
+end)
+
+local BreakIn2 = PhantomForcesWindow:NewSection("Equip Item")
+
+BreakIn2:CreateDropdown("Equip Item", ItemsTable, 2, function(Value)SelectedItemEquip = Value
+end
+
+BreakIn2:CreateButton("Equip", function()EquipItem(SelectedItemEquip)
+end
+
+local BreakIn2 = PhantomForcesWindow:NewSection("Unequip Item")
+
+BreakIn2:CreateDropdown("Unequip Item", ItemsTable, 2, function(Value)SelectedItemUnequip = Value
+end
+
+BreakIn2:CreateButton("Unequip", function()UnequipItem(SelectedItemUnequip)
+end
 
 Notify("Heal All Gui", "Loaded Script!", "rbxassetid://4483345998", 5)
