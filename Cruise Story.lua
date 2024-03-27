2cc28b44f8a5d2a62deaf315f96f710f4d5b2793
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-03-27 20:40:52 +0700

    Update Cruise Story.lua

--- Cruise Story.lua
+++ Cruise Story.lua
@@ -1,5 +1,23 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 local OrionLib = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobHubV1/NoobHubV1/main/OrionLib.lua'))()
+local ScriptLoaded = false
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remotes = ReplicatedStorage:WaitForChild("Remotes")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character
+local Humanoid = Character.Humanoid
+local WalkSpeed = Humanoid.WalkSpeed
+local JumpPower = Humanoid.JumpPower
+
+local funtion GiveMoney(Amount)Remotes.giveCashEvent:FireServer(Amount)
+end
+
+local function HealYourself()Remotes.healPlayerEvent:FireServer(math.huge)
+end
+
+local function GiveItem(Item)Remotes.buyItemEvent:FireServer(tostring(Item:gsub(" ", "")), "Cash", 0)
+end
 
 local function Notify(Name, Content, Image, Time)OrionLib:MakeNotification({
                                                          Name = Name,
@@ -24,63 +42,39 @@ local PhantomForcesWindow = Library:NewWindow("NoobHubV1 Hub")
 
 local CruiseStory = PhantomForcesWindow:NewSection("Item")
 
-CruiseStory:CreateButton("Ice Cream", function()local A_1 = "Icecream"
-local A_2 = "Cash"
-local A_3 = 20
-local GetItem = game.ReplicatedStorage.Remotes.buyItemEvent
-GetItem:FireServer(A_1, A_2, A_3)
+local MoneyTab = PhantomForcesWindow:NewSection("Money")
+
+local GodMode = PhantomForcesWindow:NewSection("God Mode")
+
+local PlayerTab = PhantomForcesWindow:NewSection("Player")
+
+CruiseStory:CreateDropdown("Item", {"Sand wich","Ice cream","Pizza","Bloxy cola"}, 2, function(Value)SelectedItem = Value
 end)
 
-CruiseStory:CreateButton("Sand Wich", function()local A_1 = "Sandwich"
-local A_2 = "Cash"
-local A_3 = 15
-local GetItem = game.ReplicatedStorage.Remotes.buyItemEvent
-GetItem:FireServer(A_1, A_2, A_3)
+MoneyTab:CreateTextbox("Money Amount", function(Amount)MoneyAmount = Amount
 end)
 
-CruiseStory:CreateButton("Cola", function()local A_1 = "Bloxycola"
-local A_2 = "Cash"
-local A_3 = 10
-local GetItem = game.ReplicatedStorage.Remotes.buyItemEvent
-GetItem:FireServer(A_1, A_2, A_3)
+GodMode:CreateButton("Heal Yourself", function()HealYourself()
 end)
 
-CruiseStory:CreateButton("Pizza", function()local A_1 = "Pizza"
-local A_2 = "Cash"
-local A_3 = 25
-local GetItem = game.ReplicatedStorage.Remotes.buyItemEvent
-GetItem:FireServer(A_1, A_2, A_3)
+PlayerTab:CreateTextbox("Walk Speed", function(WalkSpeed)WalkSpeed = WalkSpeed
 end)
 
-local CruiseStory = PhantomForcesWindow:NewSection("Money")
-
-CruiseStory:CreateTextbox("Get Money Amount", function(Amount)GetMoneyAmount = Amount
+CruiseStory:CreateButton("Get Item", function()GiveItem(SelectedItem)
 end)
 
-CruiseStory:CreateButton("Get Money", function()local A_1 = GetMoneyAmount
-local GetMoney = game.ReplicatedStorage.Remotes.giveCashEvent
-GetMoney:FireServer(A_1)
+MoneyTab:CreateButton("Get Money", function()GiveMoney(MoneyAmount)
 end)
 
-local CruiseStory = PhantomForcesWindow:NewSection("God Mode")
-
-CruiseStory:CreateButton("Heal Yourself", function()game.ReplicatedStorage.Remotes.healPlayerEvent:FireServer(math.huge)
-end)
-
-CruiseStory:CreateToggle("God Mode Yourself", function(Value)_G.Heal = Value
-while _G.Heal do
-wait()
-game.ReplicatedStorage.Remotes.healPlayerEvent:FireServer(math.huge)
-wait()
+GodMode:CreateToggle("God Mode Yourself", function(State)getgenv().HealLoop = State
+while HealLoop do
+HealYourself()
+task.wait()
 end
 end)
 
-local CruiseStory = PhantomForcesWindow:NewSection("Player")
-
-CruiseStory:CreateTextbox("Walk Speed", function(Amount)game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Amount
+PlayerTab:CreateTextbox("Jump Power", function(JumpPower)JumpPower = JumpPower
 end)
 
-CruiseStory:CreateTextbox("Jump Power", function(Amount)game.Players.LocalPlayer.Character.Humanoid.JumpPower = Amount
-end)
-
-Notify("NoobHubV1 Hub", "Loaded Script!", "rbxassetid://4483345998", 5)
+Notify("NoobHubV1 Hub", "Loaded Script!", "rbxassetid://4483345998", 10)
+ScriptLoaded = true
