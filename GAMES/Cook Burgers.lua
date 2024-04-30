1e490c5c3cfb1fe17d81b8e25f1f3c8f5558e77d
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-04-30 19:33:45 +0700

    Update Cook Burgers.lua

--- GAMES/Cook Burgers.lua
+++ GAMES/Cook Burgers.lua
@@ -1202,7 +1202,62 @@ local function DoCommand(Command)
 end
 local Sound = Instance.new(("Sound"),(workspace))
 Sound.SoundId = ("rbxassetid://1053296915") Sound:Play()
-game:GetService("StarterGui"):SetCore("SendNotification", {Title = ("Cook burgers admin V1");Text = ("Thanks for using made by NoobHubV1");})
+
+function Tween(Obj, Prop, New, Time)
+	if not Time then
+		Time = .5
+	end
+	local TweenService = game:GetService("TweenService")
+	local info = TweenInfo.new(
+		Time, 
+		Enum.EasingStyle.Quart, 
+		Enum.EasingDirection.Out, 
+		0, 
+		false,
+		0
+	)
+	local propertyTable = {
+		[Prop] = New,
+	}
+
+	TweenService:Create(Obj, info, propertyTable):Play()
+end
+
+function Notify(Text,Dur)
+	task.spawn(function()
+		if not Dur then
+			Dur = 1.5
+		end
+		local Notif = Instance.new("ScreenGui")
+		local Frame_1 = Instance.new("Frame")
+		local TextLabel = Instance.new("TextLabel")
+		Notif.Parent = (game:GetService("CoreGui") or gethui())
+		Notif.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+		Frame_1.Parent = Notif
+		Frame_1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+		Frame_1.BackgroundTransparency=1
+		Frame_1.BorderSizePixel = 0
+		Frame_1.Position = UDim2.new(0, 0, 0.0500000007, 0)
+		Frame_1.Size = UDim2.new(1, 0, 0.100000001, 0)
+		TextLabel.Parent = Frame_1
+		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.BackgroundTransparency = 1.000
+		TextLabel.TextTransparency =1
+		TextLabel.Size = UDim2.new(1, 0, 1, 0)
+		TextLabel.Font = Enum.Font.Highway
+		TextLabel.Text = Text or "Text not found"
+		TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+		TextLabel.TextSize = 21.000
+		Tween(Frame_1,"BackgroundTransparency",0.350,.5)
+		Tween(TextLabel,"TextTransparency",0,.5)
+		wait(Dur+.7)
+		Tween(Frame_1,"BackgroundTransparency",1,.5)
+		Tween(TextLabel,"TextTransparency",1,.5)
+		wait(.7)
+		Notif:Destroy()
+	end)
+	return
+end
 
 lp.Chatted:connect(function(Msg) -- haha chat go brrrrr
 	DoCommand(Msg:lower())
@@ -1278,3 +1333,5 @@ spawn(function()
 end)
 
 -- hi, im NoobHubV1 if you see me in a game
+
+Notify("(Cook Burgers Admin V1) Script Loaded!",5)
