f0f612bfaad020a9174aadadccc5bb10b3b54909
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-05-01 14:30:00 +0700

    Update Tiger Admin.lua

--- Tiger Admin.lua
+++ Tiger Admin.lua
@@ -1,26 +1,3 @@
-if game.PlaceId ~= 155615604 then
-
-local OrionLib = loadstring(Game:HttpGetAsync(("https://raw.githubusercontent.com/NoobHubV1/RobloxScripts/main/OrionLib.lua")))()
-
-local function Notify(Name, Content, Time)
-	OrionLib:MakeNotification({
-		Name = Name,
-		Content = Content,
-		Image = "rbxassetid://4483345998",
-		Time = Time
-	})
-end
-Notify("NoobHubV1 Hub", "Tiger Admin Loading Script.", 3)
-
-        wait(3)
-
-Notify("NoobHubV1 Hub", "Tiger Admin Loading Script..", 3)
-
-        wait(3)
-
-Notify("NoobHubV1 Hub", "Tiger Admin Loading Script...", 3)
-
-        wait(3)
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/dalloc2/Roblox/main/TigerAdmin.lua'))()
+if game.PlaceId ~= 73885730 then
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/dalloc2/Roblox/main/TigerAdmin.lua'))()
 end
