6dbef6d9090526cca246233f9406c6a04d87390b
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-05-21 15:38:15 +0700

    Create Evade.lua

---
+++ GAMES/Evade.lua
@@ -0,0 +1,14 @@
+local Library = loadstring(Game:HttpGetAsync(("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard")))()
+
+local Window = Library:NewWindow("NoobHubV1 Hub")
+
+local Evade = Window:NewSection("Main")
+
+Evade:CreateButton("BinCood Hub", function()loadstring(game:HttpGet('https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Evade.txt'))()
+end)
+
+Evade:CreateButton("Ghost Hub Evade", function()loadstring(Game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Evade'))()
+end)
+
+Evade:CreateButton("Speed Hub", function()loadstring(game:HttpGet('https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Speed.txt'))()
+end)
