f8b94c9b82bfde131a93a191bc71046a97271927
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-06-23 11:14:07 +0700

    Update Evade.lua

--- GAMES/Evade.lua
+++ GAMES/Evade.lua
@@ -1,14 +1,27 @@
 local Library = loadstring(Game:HttpGetAsync(("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard")))()
 
+local function LoadstringHttps(Https)
+        loadstring(game:HttpGet(Https))()
+end
+
 local Window = Library:NewWindow("NoobHubV1 Hub")
 
 local Evade = Window:NewSection("Main")
 
-Evade:CreateButton("BinCood Hub", function()loadstring(game:HttpGet('https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Evade.txt'))()
+Evade:CreateButton("BinCood Hub", function()LoadstringHttps("https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Evade.txt")
 end)
 
-Evade:CreateButton("Ghost Hub Evade", function()loadstring(Game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Evade'))()
+Evade:CreateButton("Ghost Hub Evade", function()LoadstringHttps("https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Evade")
 end)
 
-Evade:CreateButton("Speed Hub", function()loadstring(game:HttpGet('https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Speed.txt'))()
+Evade:CreateButton("Speed Hub", function()LoadstringHttps("https://raw.githubusercontent.com/Infinity2346/Tect-Menu/main/Speed.txt")
+end)
+
+Evade:CreateButton("Kills Hub", function()LoadstringHttps("https://raw.githubusercontent.com/Toilet71/GeneralUse0-3/main/Script/Evade%20v2/Main%20Gui/.Loader.lua")
+end)
+
+Evade:CreateButton("Tbao Hub", function()LoadstringHttps("https://raw.githubusercontent.com/tbao143/thaibao/main/TbaoHubEvade")
+end)
+
+Evade:CreateButton("Hydra Network", function()LoadstringHttps("https://raw.githubusercontent.com/Robobo2022/script/main/Main.lua")
 end)
