8f15d413c179af589574128b4cfea98d15640637
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-06-30 16:42:27 +0700

    Update Horrors RNG.lua

--- GAMES/Horrors RNG.lua
+++ GAMES/Horrors RNG.lua
@@ -1,12 +1,15 @@
-local Library = spawn(loadstring(Game:HttpGet('https://github.com/bloodball/-back-ups-for-libs/blob/main/wizard?raw=true')))
+local Library = loadstring(Game:HttpGetAsync(("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard")))()
 
 local Window = Library:NewWindow("NoobHubV1 Hub")
 
 local Section = Window:NewSection("Main")
 
-Section:CreateToggle("Auto Luck", function(Value)getgenv().Loop = Value
+Section:CreateTextbox("Time Loop", function(Value)TimeLoop = Value
+end)
+
+Section:CreateToggle("Auto Luck", function(State)getgenv().Loop = State
 while Loop do
 game:GetService("ReplicatedStorage").Events.InventoryEvent:FireServer("Equip","Super Luck Potion","Usable")
-task.wait()
+task.wait(TimeLoop)
 end
 end)
