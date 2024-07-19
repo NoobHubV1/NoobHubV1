b2b93b76e409f92bc61c35f0305db2e9b4365d1b
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-07-19 13:59:07 +0700

    Update Zombie Attack.lua

--- GAMES/Zombie Attack.lua
+++ GAMES/Zombie Attack.lua
@@ -1,1 +1,66 @@
-loadstring(game:HttpGet(('https://gist.githubusercontent.com/NoobHubV1/6c649fc5740e93ccee70fe5c3734f7d1/raw/49f03d0a93e724b4dc09ef860e5bd9e59b99222f/Zombie%20Attack.lua')))()
+local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard'))()
+
+local function AutoFarm(State)
+        local groundDistance = 8
+local Player = game:GetService("Players").LocalPlayer
+local function getNearest()
+local nearest, dist = nil, 99999
+for _,v in pairs(game.Workspace.BossFolder:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+for _,v in pairs(game.Workspace.enemies:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+return nearest
+end
+_G.farm2 = State
+Player.Chatted:Connect(function(msg)
+if msg == "/e ;autofarm false" then
+_G.farm2 = false
+elseif msg == "autofarm true" then
+_G.farm2 = true
+end
+end)
+_G.globalTarget = nil
+game:GetService("RunService").RenderStepped:Connect(function()
+if(_G.farm2==true)then
+local target = getNearest()
+if(target~=nil)then
+Player.Character.HumanoidRootPart.CFrame = (target.HumanoidRootPart.CFrame * CFrame.new(0, groundDistance, 9))
+_G.globalTarget = target
+end
+end
+end)
+spawn(function()
+while wait() do
+game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+game.Players.LocalPlayer.Character.Torso.Velocity = Vector3.new(0,0,0)
+end
+end)
+while wait() do
+if(_G.farm2==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
+local target = _G.globalTarget
+game.ReplicatedStorage.Gun:FireServer({["Normal"] = Vector3.new(0, 0, 0), ["Direction"] = target.Head.Position, ["Name"] = Player.Character:FindFirstChildOfClass("Tool").Name, ["Hit"] = target.Head, ["Origin"] = target.Head.Position, ["Pos"] = target.Head.Position,})
+wait()
+end
+end
+end
+
+local Window = Library:NewWindow("NoobHubV1 Hub")
+
+local Section = Window:NewSection("Main")
+
+Section:CreateToggle("Auto Farm", function(Value)AutoFarm(Value)
+end)
