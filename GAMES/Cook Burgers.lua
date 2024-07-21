571e68809f07964750dbc0bd2a5490af415ffd54
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-07-21 08:58:40 +0700

    Update Cook Burgers.lua

--- GAMES/Cook Burgers.lua
+++ GAMES/Cook Burgers.lua
@@ -36,10 +36,10 @@ print([[
 	.fix | Refresh the server
 	.slave (Plr) | Make a player follow you until they get to you
 	.jail (Plr) | Lock a player until they reset
-	.loopkill (Plr) | Player Get Loopkill (if Player ChangeServer or Leave Map)
-	.unloopkill (Plr) | Player unloopkill
+	.loopkill (Plr) | Player Get Loopkill (if Player ChangeServer or Leave Map) (Can do ".unloopkill")
 	.void (Plr) | Player Tp The Void
 	.destroy (Plr) | Player Destroy (if Player Rejoin)
+	.sit (Plr) | Sit a Player
 \\
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -114,13 +114,15 @@ local Void = Instance.new("TextButton")
 local UICorner_23 = Instance.new("UICorner")
 local Destroy = Instance.new("TextButton")
 local UICorner_24 = Instance.new("UICorner")
-local target = Instance.new("TextBox")
+local Sit = Instance.new("TextButton")
 local UICorner_25 = Instance.new("UICorner")
+local target = Instance.new("TextBox")
+local UICorner_26 = Instance.new("UICorner")
 local UIGradient_2 = Instance.new("UIGradient")
 local Refresh = Instance.new("TextButton")
-local UICorner_26 = Instance.new("UICorner")
-local search = Instance.new("TextBox")
 local UICorner_27 = Instance.new("UICorner")
+local search = Instance.new("TextBox")
+local UICorner_28 = Instance.new("UICorner")
 
 --Properties:
 
@@ -590,6 +592,22 @@ Destroy.TextSize = 23.000
 UICorner_24.CornerRadius = UDim.new(0, 4)
 UICorner_24.Parent = Destroy
 
+Sit.Name = "Sit"
+Sit.Parent = ScrollingFrame
+Sit.BackgroundColor3 = Color3.fromRGB(172, 172, 172)
+Sit.BackgroundTransparency = 0.500
+Sit.BorderSizePixel = 0
+Sit.Position = UDim2.new(0.0351677425, 0, 0.190713778, 0)
+Sit.Size = UDim2.new(0, 131, 0, 40)
+Sit.ZIndex = 3
+Sit.Font = Enum.Font.SourceSansLight
+Sit.Text = "Sit"
+Sit.TextColor3 = Color3.fromRGB(255, 255, 255)
+Sit.TextSize = 23.000
+
+UICorner_25.CornerRadius = UDim.new(0, 4)
+UICorner_25.Parent = Sit
+
 target.Name = "target"
 target.Parent = Main
 target.BackgroundColor3 = Color3.fromRGB(172, 172, 172)
@@ -602,8 +620,8 @@ target.Text = ""
 target.TextColor3 = Color3.fromRGB(255, 255, 255)
 target.TextSize = 23.000
 
-UICorner_25.CornerRadius = UDim.new(0, 4)
-UICorner_25.Parent = target
+UICorner_26.CornerRadius = UDim.new(0, 4)
+UICorner_26.Parent = target
 
 UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(255, 255, 255)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(122, 122, 122))}
 UIGradient_2.Rotation = 90
@@ -621,8 +639,8 @@ Refresh.TextColor3 = Color3.fromRGB(255, 255, 255)
 Refresh.TextSize = 26.000
 Refresh.TextWrapped = true
 
-UICorner_26.CornerRadius = UDim.new(0, 4)
-UICorner_26.Parent = Refresh
+UICorner_27.CornerRadius = UDim.new(0, 4)
+UICorner_27.Parent = Refresh
 
 search.Name = "search"
 search.Parent = Main
@@ -636,8 +654,8 @@ search.Text = ""
 search.TextColor3 = Color3.fromRGB(255, 255, 255)
 search.TextSize = 23.000
 
-UICorner_27.CornerRadius = UDim.new(0, 4)
-UICorner_27.Parent = search
+UICorner_28.CornerRadius = UDim.new(0, 4)
+UICorner_28.Parent = search
 
 local UserInputService = game:GetService("UserInputService")
 local dragging,dragInput,dragStart,startPos
@@ -849,6 +867,20 @@ local function DoCommand(Command)
 						end
 						player.Character:FindFirstChildOfClass('Humanoid').JumpHeight =(100)
 						player.Character:FindFirstChildOfClass('Humanoid').JumpPower =(100)
+					end
+				end
+			end)
+		elseif (string.sub(Command,1,#".sit") == ".sit") then
+			local player = getPlayer(string.sub(Command,#".sit" + 2))
+			pcall(function()
+				if (player ~= nil and player.Character ~= nil) then
+					if (player.Character:FindFirstChildOfClass("Part") ~= nil) then
+						for _, Part in pairs(player.Character:GetDescendants()) do
+							if (Part:IsA("BasePart")) then
+								Remote:FireServer(Part,lp)
+							end
+						end
+						player.Character.Humanoid.Sit = true
 					end
 				end
 			end)
