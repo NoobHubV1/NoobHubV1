283b7bfac4caf057615cae0278d6ad35eff5795c
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-07-29 14:03:08 +0700

    Update Prison Life.lua

--- GAMES/Prison Life.lua
+++ GAMES/Prison Life.lua
@@ -59,8 +59,12 @@ local function Notif(Text,Dur)
 	return
 end
 
-local function criminal()
-	LCS = game.Workspace["Criminals Spawn"].SpawnLocation
+local function ChangeTeam(Team)
+	if Team == game.Teams.Criminals then
+		workspace.Remote.TeamEvent:FireServer("Bright blue")
+		local LP = game.Players.LocalPlayer
+    local RE = LP.Character.HumanoidRootPart.Position
+    LCS = game.Workspace["Criminals Spawn"].SpawnLocation
     LCS.CanCollide = false
     LCS.Size = Vector3.new(51.05, 24.12, 54.76)
     LCS.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
@@ -69,14 +73,6 @@ local function criminal()
     LCS.CFrame = CFrame.new(-920.510803, 92.2271957, 2138.27002, 0, 0, -1, 0, 1, 0, 1, 0, 0)
     LCS.Size = Vector3.new(6, 0.2, 6)
     LCS.Transparency = 0
-end
-
-local function ChangeTeam(Team)
-	if Team == game.Teams.Criminals then
-		workspace.Remote.TeamEvent:FireServer("Bright blue")
-		local LP = game.Players.LocalPlayer
-    local RE = LP.Character.HumanoidRootPart.Position
-    criminal()
     task.wait(0.74)
     LP.Character.HumanoidRootPart.CFrame = CFrame.new(RE)
 	elseif Team == game.Teams.Inmates then
