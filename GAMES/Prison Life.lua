316bf60ed5ae532bc3c85bba2de4eb89430db8f1
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-04 10:51:52 +0700

    Update Prison Life.lua

--- GAMES/Prison Life.lua
+++ GAMES/Prison Life.lua
@@ -122,6 +122,52 @@ local function UnequipTool()
 	game.Players.LocalPlayer.Character.Humanoid:UnequipTools()
 end
 
+local function SilentAim()
+	local Players = game.Players
+local LocalPlayer = Players.LocalPlayer
+local GetPlayers = Players.GetPlayers
+local Camera = workspace.CurrentCamera
+local WTSP = Camera.WorldToScreenPoint
+local FindFirstChild = game.FindFirstChild
+local Vector2_new = Vector2.new
+local Mouse = LocalPlayer.GetMouse(LocalPlayer)
+function ClosestChar()
+    local Max, Close = math.huge
+    for I,V in pairs(GetPlayers(Players)) do
+        if V ~= LocalPlayer and V.Team ~= LocalPlayer.Team and V.Character then
+            local Head = FindFirstChild(V.Character, "Head")
+            if Head then
+                local Pos, OnScreen = WTSP(Camera, Head.Position)
+                if OnScreen then
+                    local Dist = (Vector2_new(Pos.X, Pos.Y) - Vector2_new(Mouse.X, Mouse.Y)).Magnitude
+                    if Dist < Max then
+                        Max = Dist
+                        Close = V.Character
+                    end
+                end
+            end
+        end
+    end
+    return Close
+end
+
+local MT = getrawmetatable(game)
+local __namecall = MT.__namecall
+setreadonly(MT, false)
+MT.__namecall = newcclosure(function(self, ...)
+    local Method = getnamecallmethod()
+    if Method == "FindPartOnRay" and not checkcaller() and tostring(getfenv(0).script) == "GunInterface" then
+        local Character = ClosestChar()
+        if Character then
+            return Character.Head, Character.Head.Position
+        end
+    end
+
+    return __namecall(self, ...)
+end)
+setreadonly(MT, true)
+end
+
 local AllGuns = function()
 	if BuyGamepass then
              GiveItem("Remington 870")
@@ -235,7 +281,7 @@ end)
 
 local PrisonLife = Window:NewSection("Item")
 
-PrisonLife:CreateDropdown("Item", {"Shotgun","AK-47","M9","Hammer","Knife","M4A1"}, 1, function(Value)SelectedItem = Value
+PrisonLife:CreateDropdown("Item", {"Remington 870","AK-47","M9","Hammer","Knife","M4A1"}, 1, function(Value)SelectedItem = Value
 end)
 
 PrisonLife:CreateButton("Give Item", function()GiveItem(SelectedItem)
@@ -245,6 +291,9 @@ PrisonLife:CreateToggle("Auto All Guns", function(Value)AutoGuns(Value)
 end)
 
 PrisonLife:CreateButton("All Guns", function()AllGuns()
+end)
+
+PrisonLife:CreateButton("Silent Aim", function()SilentAim()
 end)
 
 PrisonLife:CreateButton("Server Crash", function()ServerCrash()
