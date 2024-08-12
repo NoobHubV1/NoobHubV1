34b5e39ea8ee20734e684aba25c349a910769663
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-12 21:35:55 +0700

    Update Gun Ball.lua

--- GAMES/Gun Ball.lua
+++ GAMES/Gun Ball.lua
@@ -1,77 +1,4 @@
-local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard'))()
-
-local function Tween(Obj, Prop, New, Time)
-	if not Time then
-		Time = .5
-	end
-	local TweenService = game:GetService("TweenService")
-	local info = TweenInfo.new(
-		Time, 
-		Enum.EasingStyle.Quart, 
-		Enum.EasingDirection.Out, 
-		0, 
-		false,
-		0
-	)
-	local propertyTable = {
-		[Prop] = New,
-	}
-
-	TweenService:Create(Obj, info, propertyTable):Play()
-end
-
-local function Notif(Text,Dur)
-	task.spawn(function()
-		if not Dur then
-			Dur = 1.5
-		end
-		local Notif = Instance.new("ScreenGui")
-		local Frame_1 = Instance.new("Frame")
-		local TextLabel = Instance.new("TextLabel")
-		Notif.Parent = (game:GetService("CoreGui") or gethui())
-		Notif.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-		Frame_1.Parent = Notif
-		Frame_1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-		Frame_1.BackgroundTransparency=1
-		Frame_1.BorderSizePixel = 0
-		Frame_1.Position = UDim2.new(0, 0, 0.0500000007, 0)
-		Frame_1.Size = UDim2.new(1, 0, 0.100000001, 0)
-		TextLabel.Parent = Frame_1
-		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel.BackgroundTransparency = 1.000
-		TextLabel.TextTransparency =1
-		TextLabel.Size = UDim2.new(1, 0, 1, 0)
-		TextLabel.Font = Enum.Font.Highway
-		TextLabel.Text = Text or "Text not found"
-		TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel.TextSize = 21.000
-		Tween(Frame_1,"BackgroundTransparency",0.350,.5)
-		Tween(TextLabel,"TextTransparency",0,.5)
-		wait(Dur+.7)
-		Tween(Frame_1,"BackgroundTransparency",1,.5)
-		Tween(TextLabel,"TextTransparency",1,.5)
-		wait(.7)
-		Notif:Destroy()
-	end)
-	return
-end
-
-local function Parry()
-        game:GetService("ReplicatedStorage").RemoteEvent:FireServer({["name"] = "defense",["origin"] = "balltargets"},{})
-end
-
-local SpamParry = function(State)
-        getgenv().SpamParry = State
-        while SpamParry do
-        for i = 1, 5 do
-        Parry()
-        end
-        task.wait()
-        end
-end
-
-local function SpamParryButton()
-        -- Made by NoobHubV1 
+-- Made by NoobHubV1
  
  -- Instances:
 
@@ -194,6 +121,7 @@ local function MKANE_fake_script() -- re2.LocalScript
 		while _G.Loop == true do
 			Parry()
                         task.wait()
+			Notif("Spam Parry Enabled")
 		end
 	end)
 end
@@ -258,20 +186,3 @@ local function VYXMZ_fake_script() -- Main.drag
 	end)
 end
 coroutine.wrap(VYXMZ_fake_script)()
-end
-
-local Window = Library:NewWindow("NoobHubV1 Hub")
-
-local GunBall = Window:NewSection("Main")
-
-GunBall:CreateButton("Spam Parry On", function()SpamParry(true) task.wait(0.1)
-Notif("Spam Parry Enabled")
-end)
-
-GunBall:CreateButton("Spam Parry Off", function()SpamParry(false)
-Notif("Spam Parry Disabled")
-end)
-
-GunBall:CreateButton("Spam Parry Button", function()SpamParryButton()
-Notif("Spam Parry Button Loaded!",5)
-end)
