9ae421e7b76ca15bed415ffe73936b38000f14c4
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-19 18:58:22 +0700

    Update Prison Life.lua

--- GAMES/Prison Life.lua
+++ GAMES/Prison Life.lua
@@ -1572,6 +1572,103 @@ local CFrame = function(X, Y, Z)
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(X, Y, Z)
 end
 
+local function ArrestPlr()
+	-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
+local TextButton = Instance.new("TextButton")
+
+--Properties:
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
+Frame.Position = UDim2.new(0.0799492374, 0, 0.483815432, 0)
+Frame.Size = UDim2.new(0, 292, 0, 171)
+Frame.Active = true
+Frame.Archivable = true
+Frame.Draggable = true
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.157534242, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Arrest Script"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextStrokeTransparency = 0.000
+TextLabel.TextWrapped = true
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.Position = UDim2.new(0.157534242, 0, 0.350877196, 0)
+TextBox.Size = UDim2.new(0, 200, 0, 50)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.Text = ""
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(170, 0, 255)
+TextButton.Position = UDim2.new(0.311643839, 0, 0.713450253, 0)
+TextButton.Size = UDim2.new(0, 109, 0, 39)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "ARREST"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+
+local plr = game.Players.LocalPlayer
+
+function GetPlayer(String)
+        if not String then return end
+	local Yes = {}
+	for _, Player in ipairs(game.Players:GetPlayers()) do
+		if string.lower(Player.Name):match(string.lower(String)) or string.lower(Player.DisplayName):match(string.lower(String)) then
+			table.insert(Yes, Player)
+		end
+	end
+	if #Yes > 0 then
+		return Yes[1]
+	elseif #Yes < 1 then
+		return nil
+	end
+end
+
+local function Arrest(Player)
+        workspace.Remote.arrest:InvokeServer(Player.Character.Head)
+end
+
+local function Goto(Player)
+        plr.Character.HumanoidRootPart.CFrame = Player.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, 0)
+end
+
+TextButton.MouseButton1Click:Connect(function()
+local Target = TextBox.Text
+local savedcf = plr.Character.HumanoidRootPart.Position
+for i = 1, 20 do
+Goto(GetPlayer(Target))
+Arrest(GetPlayer(Target))
+task.wait()
+end
+plr.Character.HumanoidRootPart.CFrame = CFrame.new(savedcf)
+end)
+end
+
 local Window = Library:NewWindow("NoobHubV1 Hub")
 
 local PrisonLife = Window:NewSection("Main")
@@ -1724,6 +1821,9 @@ PrisonLife:CreateButton("Kill Plr", function()KillPlr()
 end)
 
 PrisonLife:CreateButton("Loop Kill Plr", function()LoopkillPlr()
+end)
+
+PrisonLife:CreateButton("Arrest Plr", function()ArrestPlr()
 end)
 
 Notif("(Prison Life Script By NoobHubV1) Script Loaded!)")
