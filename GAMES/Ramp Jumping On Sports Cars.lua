3cb95f87ee2603080a280962f0b4b0f43a19b933
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-08-23 18:57:36 +0700

    Update Ramp Jumping On Sports Cars.lua

--- GAMES/Ramp Jumping On Sports Cars.lua
+++ GAMES/Ramp Jumping On Sports Cars.lua
@@ -97,7 +97,7 @@ elseif strl == "me" then
        end  
    else
        for i,v in pairs(game.Players:GetPlayers()) do
-           if v.Name:lower():sub(1, #String) == String:lower() then
+           if v.Name:lower():sub(1, #String) == String:lower() or v.DisplayName:lower():sub(1, #String) == String:lower() then
                table.insert(Found,v.Name)
            end
        end    
