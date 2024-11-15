faf648eada8c769c9a8c782169880491c0568377
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-11-15 19:49:26 +0700

    Update Zombie Attack.lua

--- GAMES/Zombie Attack.lua
+++ GAMES/Zombie Attack.lua
@@ -26,34 +26,19 @@ end
 return nearest
 end
 _G.farm2 = State
-Player.Chatted:Connect(function(msg)
-if msg == "/e ;autofarm false" then
-_G.farm2 = false
-elseif msg == "autofarm true" then
-_G.farm2 = true
-end
-end)
 _G.globalTarget = nil
 game:GetService("RunService").RenderStepped:Connect(function()
 if(_G.farm2==true)then
 local target = getNearest()
 if(target~=nil)then
-Player.Character.HumanoidRootPart.CFrame = (target.HumanoidRootPart.CFrame * CFrame.new(0, groundDistance, 9))
 _G.globalTarget = target
 end
 end
 end)
-spawn(function()
-while wait() do
-game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity = Vector3.new(0,0,0)
-game.Players.LocalPlayer.Character.Torso.Velocity = Vector3.new(0,0,0)
-end
-end)
-while wait() do
+while task.wait() do
 if(_G.farm2==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
 local target = _G.globalTarget
 game.ReplicatedStorage.Gun:FireServer({["Normal"] = Vector3.new(0, 0, 0), ["Direction"] = target.Head.Position, ["Name"] = Player.Character:FindFirstChildOfClass("Tool").Name, ["Hit"] = target.Head, ["Origin"] = target.Head.Position, ["Pos"] = target.Head.Position,})
-wait()
 end
 end
 end
@@ -62,5 +47,5 @@ local Window = Library:NewWindow("NoobHubV1 Hub")
 
 local Section = Window:NewSection("Main")
 
-Section:CreateToggle("Auto Farm", function(Value)AutoFarm(Value)
+Section:CreateToggle("Auto Farm", function(Value)AutoFarm(Value, AutoTeleport)
 end)
