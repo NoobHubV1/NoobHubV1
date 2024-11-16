4c726671127ad78a518101e75ed37335aae0e662
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-11-16 09:35:23 +0700

    Update Zombie Attack.lua

--- GAMES/Zombie Attack.lua
+++ GAMES/Zombie Attack.lua
@@ -1,8 +1,5 @@
 local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard'))()
 
-local function AutoFarm(State)
-        local groundDistance = 8
-local Player = game:GetService("Players").LocalPlayer
 local function getNearest()
 local nearest, dist = nil, 99999
 for _,v in pairs(game.Workspace.BossFolder:GetChildren()) do
@@ -25,6 +22,10 @@ end
 end
 return nearest
 end
+
+local function AutoFarm(State)
+        local groundDistance = 8
+local Player = game:GetService("Players").LocalPlayer
 _G.farm2 = State
 _G.globalTarget = nil
 game:GetService("RunService").RenderStepped:Connect(function()
@@ -38,11 +39,89 @@ end)
 while task.wait() do
 if(_G.farm2==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
 local target = _G.globalTarget
-for i = 1,10 do
+for i = 1,7 do
 game.ReplicatedStorage.Gun:FireServer({["Normal"] = Vector3.new(0, 0, 0), ["Direction"] = target.Head.Position, ["Name"] = Player.Character:FindFirstChildOfClass("Tool").Name, ["Hit"] = target.Head, ["Origin"] = target.Head.Position, ["Pos"] = target.Head.Position,})
 end
 end
 end
+end
+
+local function AutoTeleport(State)
+        local Player = game:GetService("Players").LocalPlayer
+local function getNearest()
+local nearest, dist = nil, 99999
+for _,v in pairs(game.Workspace.BossFolder:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+for _,v in pairs(game.Workspace.enemies:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+return nearest
+end
+_G.Teleport2 = State
+_G.globalTarget = nil
+game:GetService("RunService").RenderStepped:Connect(function()
+if(_G.Teleport2==true)then
+local target = getNearest()
+if(target~=nil)then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = target.HumanoidRootPart.CFrame * CFrame.new(0, 12, 9)
+_G.globalTarget = target
+end
+end
+end)
+while _G.Teleport2 and wait() do
+        Player.Character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+        Player.Character.Head.Velocity = Vector3.new(0, 0, 0)
+end
+end
+
+local function AutoCamera(State)
+        local Player = game:GetService("Players").LocalPlayer
+local function getNearest()
+local nearest, dist = nil, 99999
+for _,v in pairs(game.Workspace.BossFolder:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+for _,v in pairs(game.Workspace.enemies:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+return nearest
+end
+_G.Camera2 = State
+_G.globalTarget = nil
+game:GetService("RunService").RenderStepped:Connect(function()
+if(_G.Camera2==true)then
+local target = getNearest()
+if(target~=nil)then
+workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, target.Torso.Position)
+_G.globalTarget = target
+end
+end
+end)
 end
 
 local Window = Library:NewWindow("NoobHubV1 Hub")
@@ -50,4 +129,10 @@ local Window = Library:NewWindow("NoobHubV1 Hub")
 local Section = Window:NewSection("Main")
 
 Section:CreateToggle("Auto Farm", function(Value)AutoFarm(Value)
+end)
+
+Section:CreateToggle("Auto Teleport", function(Value)AutoTeleport(Value)
+end)
+
+Section:CreateToggle("Auto Camera", function(Value)AutoCamera(Value)
 end)
