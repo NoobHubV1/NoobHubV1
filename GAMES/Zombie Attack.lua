57b60a1c04609b4d932646aa399b13341b8951da
scripts <163002311+NoobHubV1@users.noreply.github.com>
2024-11-23 09:47:45 +0700

    Update Zombie Attack.lua

--- GAMES/Zombie Attack.lua
+++ GAMES/Zombie Attack.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobHubV1/NoobHubV1/main/Wizard%20Lib.lua'))()
 
-local function AutoFarm(State)
+local function AutoShoot(State)
         local groundDistance = 8
 local Player = game:GetService("Players").LocalPlayer
 local function getNearest()
@@ -25,18 +25,18 @@ end
 end
 return nearest
 end
-_G.farm2 = State
+_G.shoot2 = State
 _G.globalTarget = nil
 game:GetService("RunService").RenderStepped:Connect(function()
-if(_G.farm2==true)then
+if(_G.shoot2==true)then
 local target = getNearest()
 if(target~=nil)then
 _G.globalTarget = target
 end
 end
 end)
-while _G.farm2 and wait() do
-if(_G.farm2==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
+while _G.shoot2 and wait() do
+if(_G.shoot2==true and _G.globalTarget~=nil and _G.globalTarget:FindFirstChild("Head") and Player.Character:FindFirstChildOfClass("Tool"))then
 local target = _G.globalTarget
 for i = 1,1 do
 game.ReplicatedStorage.Gun:FireServer({["Normal"] = Vector3.new(0, 0, 0), ["Direction"] = target.Head.Position, ["Name"] = Player.Character:FindFirstChildOfClass("Tool").Name, ["Hit"] = target.Head, ["Origin"] = target.Head.Position, ["Pos"] = target.Head.Position,})
@@ -75,7 +75,10 @@ game:GetService("RunService").RenderStepped:Connect(function()
 if(_G.Teleport2==true)then
 local target = getNearest()
 if(target~=nil)then
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = target.HumanoidRootPart.CFrame * CFrame.new(0, 15, 11)
+if game.Players.LocalPlayer.Character.Humanoid.Sit then
+        game.Players.LocalPlayer.Character.Humanoid.Sit = false
+end
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = target.HumanoidRootPart.CFrame * CFrame.new(0, 18, 13)
 _G.globalTarget = target
 end
 end
@@ -117,7 +120,43 @@ if(_G.Camera2==true)then
 local target = getNearest()
 if(target~=nil)then
 workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.Position, target.Torso.Position)
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position, target.HumanoidRootPart.Position)
+_G.globalTarget = target
+end
+end
+end)
+end
+
+local function AutoView(State)
+        local Player = game:GetService("Players").LocalPlayer
+local function getNearest()
+local nearest, dist = nil, 99999
+for _,v in pairs(game.Workspace.BossFolder:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+for _,v in pairs(game.Workspace.enemies:GetChildren()) do
+if(v:FindFirstChild("Head")~=nil)then
+local m =(Player.Character.Head.Position-v.Head.Position).magnitude
+if(m<dist)then
+dist = m
+nearest = v
+end
+end
+end
+return nearest
+end
+_G.view2 = State
+_G.globalTarget = nil
+game:GetService("RunService").RenderStepped:Connect(function()
+if(_G.view2==true)then
+local target = getNearest()
+if(target~=nil)then
+workspace.CurrentCamera.CameraSubject = target.Humanoid
 _G.globalTarget = target
 end
 end
@@ -132,17 +171,16 @@ local Section2 = Window:NewSection("Player")
 
 local Section3 = Window:NewSection("Destroy Script")
 
-Section:CreateToggle("Auto Farm", function(Value)AutoFarm(Value)
-end)
+Section:CreateToggle("Auto Farm", function(Value)AutoShoot(Value) end)
 
-Section:CreateToggle("Auto Teleport", function(Value)AutoTeleport(Value)
-end)
+Section:CreateToggle("Auto Teleport", function(Value)AutoTeleport(Value) end)
 
-Section:CreateToggle("Auto Camera", function(Value)AutoCamera(Value)
-end)
+Section:CreateToggle("Auto Camera", function(Value)AutoCamera(Value) end)
+
+Section:CreateToggle("Auto View", function(v)if v then AutoView(true) elseif not v then AutoView(false) workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character end end)
 
 Section2:CreateTextbox("Walk Speed", function(number)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = number
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = number 
 end)
 
 Section2:CreateTextbox("Jump Power", function(number)
@@ -151,7 +189,8 @@ end)
 
 Section3:CreateButton("Destroy", function()
         Library:Destroy()
-        AutoFarm(false)
+        AutoShoot(false)
         AutoTeleport(false)
         AutoCamera(false)
+        AutoView(false)
 end)
